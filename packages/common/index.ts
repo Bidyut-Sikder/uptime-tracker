a0a7b2d83f5078d4a59ceb2d209bda05b9c36f01
bidyut-sikder <bidyutsikder2001@gmail.com>
2025-03-23 20:24:36 +0600

    feat :new common package is added

---
+++ packages/common/index.ts
@@ -0,0 +1,52 @@
+export interface SignupIncomingMessage {
+  //initiated from the validator
+  ip: string;
+  publicKey: string;
+  signedMessage: string;
+  callbackId: string;
+}
+
+export interface ValidateIncomingMessage {
+  //initiated from the websoket client
+
+  callbackId: string;
+  signedMessage: string;
+  status: "Good" | "Bad";
+  latency: number;
+  websiteId: string;
+  validatorId: string;
+}
+
+export interface SignupOutgoingMessage {
+  //response from the hub
+  validatorId: string;
+  callbackId: string;
+}
+
+export interface ValidateOutgoingMessage {
+  //response from the validator
+
+  url: string;
+  callbackId: string;
+  websiteId: string;
+}
+
+export type IncomingMessage =
+  | {
+      type: "signup";
+      data: SignupIncomingMessage;
+    }
+  | {
+      type: "validate";
+      data: ValidateIncomingMessage;
+    };
+
+export type OutgoingMessage =
+  | {
+      type: "signup";
+      data: SignupOutgoingMessage;
+    }
+  | {
+      type: "validate";
+      data: ValidateOutgoingMessage;
+    };
--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -1,4 +1,4 @@
 import { PrismaClient } from "@prisma/client";
 
 export const prismaClient = new PrismaClient();
- 
+ //created once and exported in package.json ,uses everywehere by importing as dependencies in other apps
